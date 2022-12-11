cde15c712e70aa36e33318423c1f9ab912a4f7f8
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-10 21:26:01 -0300

    changes

--- src/components/QuotationBox/index.tsx
+++ src/components/QuotationBox/index.tsx
@@ -4,8 +4,6 @@ import React, { Component } from 'react';
 class QuotationBox extends React.Component {
 	constructor(props) {
 		super(props);
-		const { children } = this.props;
-	
 	}
 
 
@@ -14,7 +12,7 @@ class QuotationBox extends React.Component {
 			<div class="quotation-box">
         <div class="quotation">
           <p>
-            A inabilidade do homem caído para compreender totalmente os lembretes assombrosos de outro reino sobrenatural que ainda parecia não existir, a constante perplexidade de fenômenos inexplicáveis e vastamente metafísicos, uma propensão para escolhas morais aparentemente perversas ou más que não tiveram nenhuma firma ou medida fixa ou regra, e um senso sem nome de culpa combinado com uma suspeita que o mundo externo foi uma projeção delusiva da mente--estes foram os grandes elementos na visão do homem que os românticos sombrios opuseram para a corrente dominante do pensamento romântico.
+            {this.props.content}
           </p>
         </div>
       </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -25,6 +25,13 @@ export default class App extends Component {
 
           <SectionAboutUs />
 
+          <div className="container bg-white">
+            <QuotationBox 
+              content="
+                A inabilidade do homem caído para compreender totalmente os lembretes assombrosos de outro reino sobrenatural que ainda parecia não existir, a constante perplexidade de fenômenos inexplicáveis e vastamente metafísicos, uma propensão para escolhas morais aparentemente perversas ou más que não tiveram nenhuma firma ou medida fixa ou regra, e um senso sem nome de culpa combinado com uma suspeita que o mundo externo foi uma projeção delusiva da mente--estes foram os grandes elementos na visão do homem que os românticos sombrios opuseram para a corrente dominante do pensamento romântico.
+              "/>
+          </div>
+
           <SectionSocialMedias />
         </main>        
       </Theme>
